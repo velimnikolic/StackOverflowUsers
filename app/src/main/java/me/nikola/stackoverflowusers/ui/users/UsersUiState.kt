a7ffcbeedc17c0560016fbfb0608302d8bf20e7b
Nikola Velimirovic <velimnikolic@gmail.com>
2026-05-11 20:09:00 +0200

    Represent users screen content state

--- app/src/main/java/me/nikola/stackoverflowusers/ui/users/UsersUiState.kt
+++ app/src/main/java/me/nikola/stackoverflowusers/ui/users/UsersUiState.kt
@@ -1,5 +1,9 @@
 package me.nikola.stackoverflowusers.ui.users
 
+import me.nikola.stackoverflowusers.domain.model.StackOverflowUser
+
 data class UsersUiState(
     val isLoading: Boolean = false,
+    val users: List<StackOverflowUser> = emptyList(),
+    val errorMessage: String? = null,
 )
